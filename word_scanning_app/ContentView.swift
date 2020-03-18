b2728a710b8e46b5883bd9e2cbdcb22a164531fa
Jui Tao Tsai <jt2fx@virginia.edu>
2020-03-17 22:20:33 -0400

    setup files

---
+++ word_scanning_app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  word_scanning_app
+//
+//  Created by Jui Tao Tsai on 3/17/20.
+//  Copyright © 2020 Jui Tao Tsai. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ word_scanning_appTests/word_scanning_appTests.swift
@@ -0,0 +1,34 @@
+//
+//  word_scanning_appTests.swift
+//  word_scanning_appTests
+//
+//  Created by Jui Tao Tsai on 3/17/20.
+//  Copyright © 2020 Jui Tao Tsai. All rights reserved.
+//
+
+import XCTest
+@testable import word_scanning_app
+
+class word_scanning_appTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
